412b471b472095d6c4dbb0419424200047111eec
Baldislayer <lisov.a2005@yandex.ru>
2024-04-13 19:51:50 +0300

    replaced lab4 NerBSD

---
+++ NetBSD/lab4/lab4.c
@@ -0,0 +1,44 @@
+#include <sys/cdefs.h>
+#include <sys/module.h>
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#include <uvm/uvm.h>
+
+MODULE(MODULE_CLASS_MISC, lab4, NULL);
+#define PAGESIZE 0x1000
+extern paddr_t avail_end;
+vaddr_t va;
+struct pglist plist;
+
+static int lab4_modcmd(modcmd_t cmd, void* arg) {
+    va = uvm_km_alloc(kernel_map, PAGESIZE*10, 0, UVM_KMF_VAONLY);
+    if (va == 0) {
+        return 0;
+    }
+    
+    int error = uvm_pglistalloc(PAGESIZE*5, 0, avail_end, 0, 0, &plist, 5, 0);
+    
+    if (error) {
+        return error;
+    }
+
+    struct vm_page *page = TAILQ_FIRST(&plist);
+
+    for (int i = 0; page; ++i) {
+        pd_entry_t *ppte;
+        ppte = L2_BASE+pl2_i(va+PAGESIZE*i);
+        paddr_t pa = VM_PAGE_TO_PHYS(page);
+        printf("Page - %d\n", i+1);
+        printf("Valid - %s\n", ((*ppte & PG_V) ? "true" : "false"));
+        printf("Used - %s\n", ((*ppte & PG_U) ? "true" : "false"));
+        printf("Modified - %s\n", ((*ppte & PG_M) ? "true" : "false"));
+        printf("Physical address - 0x%lx\n", pa);
+        printf("\n");
+        page = TAILQ_NEXT(page, pageq.queue);
+   }
+   
+   uvm_pglistfree(&plist);
+   uvm_km_free(kernel_map, va, PAGESIZE*10, UVM_KMF_VAONLY);
+   
+   return 0;
+}
